9eda8541e34f4a609cc0e9addb1e27a41b3d52b3
marlenaJakubowska <mar.jakubowska90@gmail.com>
2024-05-25 15:07:02 +0200

    #2 change to camelCase

--- src/main/java/pl/edu/agh/mwo/model/PitStop.java
+++ src/main/java/pl/edu/agh/mwo/model/PitStop.java
@@ -1,28 +1,28 @@
 package pl.edu.agh.mwo.model;
 
 public class PitStop {
-    private int driver_number;
-    private double pit_duration;
-    private int lap_number;
+    private int driverNumber;
+    private double pitDuration;
+    private int lapNumber;
 
-    public PitStop(int driver_number, double pit_duration, int lap_number) {
-        this.driver_number = driver_number;
-        this.pit_duration = pit_duration;
-        this.lap_number = lap_number;
+    public PitStop(int driverNumber, double pitDuration, int lapNumber) {
+        this.driverNumber = driverNumber;
+        this.pitDuration = pitDuration;
+        this.lapNumber = lapNumber;
     }
 
     public PitStop() {
     }
 
-    public int getDriver_number() {
-        return driver_number;
+    public void setDriverNumber(int driverNumber) {
+        this.driverNumber = driverNumber;
     }
 
-    public double getPit_duration() {
-        return pit_duration;
+    public void setPitDuration(double pitDuration) {
+        this.pitDuration = pitDuration;
     }
 
-    public int getLap_number() {
-        return lap_number;
+    public void setLapNumber(int lapNumber) {
+        this.lapNumber = lapNumber;
     }
 }
