c780d99554ca674c9c1e25ad32e4014bc14f98f7
pawjamro <jamro.pawel@gmail.com>
2024-05-25 15:54:22 +0200

    #6 Jsonreadre

--- src/main/java/pl/edu/agh/mwo/App.java
+++ src/main/java/pl/edu/agh/mwo/App.java
@@ -1,7 +1,11 @@
 package pl.edu.agh.mwo;
 
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world");
+
     }
+
 }
